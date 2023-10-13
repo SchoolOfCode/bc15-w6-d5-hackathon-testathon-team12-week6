839d77aaa924d1080ab9cb26ce0c1abf2313c92e
Hannah Thorley <hannahthorley2014@gmail.com>
2023-10-13 14:11:26 +0100

    tickets 3 - 5

--- todos/todos.test.js
+++ todos/todos.test.js
@@ -35,3 +35,70 @@ test("POST /api/todos", async function () {
   // assert that there's a Content-Type response header which contains `application/json`
   expect(response.header["content-type"]).toMatch("application/json");
 });
+
+// 3. Deleting a todo
+
+test("DELETE /api/todos/some_id", async function () {
+  // Reset database for expected testing
+  const resetTable = await resetAllTables();
+
+  const fillTable = await request(app)
+    .post("/api/todos")
+    .set("content-type", "application/json")
+    .send({ task: "Washing", completionDate: "2023-10-13" })
+    .expect(201);
+  const fillTableId = fillTable.body.payload.id;
+
+  const response = await request(app)
+    .delete(`/api/todos/${fillTableId}`)
+    .expect(200);
+
+  //console.log(response);
+  // Response body success is true
+  expect(response.body.success).toBe(true);
+  // assert that the payload is an object
+  expect(typeof response.body.payload).to.equal("object");
+
+  // assert that there's a Content-Type response header which contains `application/json`
+  expect(response.header["content-type"]).toMatch("application/json");
+});
+
+// 4. Invalid request
+
+test("POST /api/todos", async function () {
+  // Reset database for expected testing
+  const resetTable = await resetAllTables();
+
+  const response = await request(app)
+    .post("/api/todos")
+    .set("content-type", "application/json")
+    .send({})
+    .expect(400);
+
+  expect(typeof response.body).toBe("object");
+  // Response body success is false
+  expect(response.body.success).toBe(false);
+  expect(response.body.error).toMatch(
+    "Please provide a 'task' and 'completionDate'"
+  );
+
+  // assert that there's a Content-Type response header which contains `application/json`
+  expect(response.header["content-type"]).toMatch("application/json");
+});
+
+// 5. Invalid request
+
+test("	DELETE /api/todos/NONEXISTENT_ID", async function () {
+  // Reset database for expected testing
+  const resetTable = await resetAllTables();
+
+  const response = await request(app).delete("/api/todos/564").expect(404);
+  expect(typeof response.body).toBe("object");
+  // Response body success is false
+  expect(response.body.success).toBe(false);
+  // assert that eror is "No user with id 564 found"
+  expect(response.body.error).toMatch("No todo with id 564 found");
+
+  // assert that there's a Content-Type response header which contains `application/json`
+  expect(response.header["content-type"]).toMatch("application/json");
+});
