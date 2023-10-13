f94390b75861c4d44eba4527a610043e3240d174
Ana Raducanu <danuta.anna@yahoo.com>
2023-10-13 16:56:07 +0100

    added code in vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["dotenv/config"],
+    exclude: ["**/node_modules/**", "**/test/**"],
   },
 });
